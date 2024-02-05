0668eb8e9af1db9074c91107f012f9dab4479069
Aahmed-Hossain <aahmdctec@gmail.com>
2024-02-05 17:20:41 +0600

    deployed and updated the crud operation

--- index.js
+++ index.js
@@ -25,19 +25,52 @@ async function run() {
   try {
     // await client.connect();
     const productsCollection = client.db("bucketBD").collection("products");
-    // const enrolledStudentCollection = client.db("alemeno-course-dashboard").collection("students");
-    // const likeCollection = client.db("alemeno-course-dashboard").collection("likes");
+    const userCollection = client.db("bucketBD").collection("users");
+    const orderCollection = client.db("bucketBD").collection("orders");
     
     app.get('/products', async(req, res)=> {
         const result = await productsCollection.find().toArray();
         res.send(result);
     });
-    // app.get('/courses/:id', async(req, res)=>{
-    //   const id = req.params.id;
-    //   const filter = { _id: new ObjectId(id) };
-    //   const result = await courseCollection.findOne(filter);
-    //   res.send(result);
-    // });
+    app.get('/products/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(filter);
+      res.send(result);
+    });
+    app.post('/addProducts', async(req, res)=> {
+      const body = req.body;
+      const result = await productsCollection.insertOne(body);
+      res.send(result);
+    });
+
+    app.post('/users/register', async(req, res)=> {
+      const body = req.body;
+      const result = await userCollection.insertOne(body);
+      res.send(result);
+    });
+    app.post('/orders', async(req, res)=> {
+      const body = req.body;
+      const result = await orderCollection.insertOne(body);
+      res.send(result);
+    });
+    app.get('/orders', async(req, res)=> {
+      const result = await orderCollection.find().toArray();
+      res.send(result);
+  });
+  app.delete('/orders/:id', async(req, res)=>{
+    const id = req.params.id;
+    console.log(id);
+    const filter = { _id: new ObjectId(id) };
+    console.log(filter);
+    const result = await orderCollection.deleteOne(filter);
+    res.send(result);
+  });
+
+  app.get('/users', async(req, res)=> {
+        const result = await userCollection.find().toArray();
+        res.send(result);
+    });
     
     // app.get("/search-course/:name", async (req, res) => {
     //   const name = req.params.name;
@@ -46,11 +79,73 @@ async function run() {
     //   res.send(result);
     // });
 
-    // app.post('/enrolledStudent', async(req,res )=> {
-    //   const body = req.body;
-    //   const result = await enrolledStudentCollection.insertOne(body);
+    const isValidBangladeshiNumber = (mobileNumber) => {
+      const result = { isValid: false, message: "" };
+    
+      if (!mobileNumber || typeof mobileNumber !== "string") {
+        result.message = "ERROR! Please enter a valid mobile number";
+        return result;
+      }
+    
+      if (!/^\+?\d+$/.test(mobileNumber)) {
+        result.message = "ERROR! Mobile number must contain only digits (0-9)";
+        return result;
+      }
+    
+      let inputNumber = mobileNumber.trim();
+    
+      const countryCode = inputNumber.startsWith("+")
+        ? inputNumber.slice(0, 3)
+        : null;
+    
+      const operatorCode = inputNumber.startsWith("+")
+        ? inputNumber.slice(3, 6)
+        : inputNumber.slice(0, 3);
+    
+      const validCountryCode = "+88";
+      const validTotalDigit = 11;
+      const validOperatorCodes = ["013", "014", "015", "016", "017", "018", "019"];
+    
+      if (countryCode) {
+        if (countryCode !== validCountryCode) {
+          result.message = `ERROR! ${countryCode} is an invalid country code for Bangladesh.`;
+          return result;
+        } else {
+          inputNumber = inputNumber.slice(3);
+        }
+      }
+    
+      if (inputNumber.length !== validTotalDigit) {
+        result.message = `ERROR! ${inputNumber} has an invalid total digit as a Bangladeshi number.`;
+        return result;
+      }
+    
+      if (!validOperatorCodes.includes(operatorCode)) {
+        result.message = `ERROR! ${operatorCode} is an invalid operator code for Bangladeshi number.`;
+        return result;
+      }
+    
+      result.isValid = true;
+      result.message = `${inputNumber} is a valid Bangladeshi mobile number.`;
+      return result;
+    };
+    
+    // app.post('/users/register', async(req,res )=> {
+    //   const {number, password} = req.body;
+    //   // const isValidNumber = isValidBangladeshiNumber(number);
+    //   if(isValidNumber.isValid){
+    //     // check user avaiale is already?
+    //     // bcrypt password 
+    //     // insert 
+    //     // genetate jwt token 
+    //     const result = await userCollection.insertOne(body);
     //   res.send(result);
+    //   res.status(200).send({success:ture,message:"user registered successfully", data:result, token:"token"})
+    //   }else{
+    //     res.status(422).send({success:false,message:isValidNumber.message, data:isValidNumber})
+    //   }
     // });
+
 
     // app.get('/students', async(req,res)=> {
     //   const result = await enrolledStudentCollection.find().toArray();
